0cfb40324f4c6b78fd122ba717325eb00689559c
pesricha <naman_p@me.iitr.ac.in>
2024-09-14 18:37:02 +0530

    Question3 solution added without profiling modifications.

--- A1/user_code.h
+++ A1/user_code.h
@@ -13,6 +13,8 @@
 
 using namespace std;
 
+//Global Variable to store the count of hashtags for each customer. To be reused in Q3.
+unordered_map<int, unordered_map<string, int>> hashtagCount;
 
 
 // #PROMPT# Split function to split a line into tokens. Use this function to split the lines read from the files.
@@ -69,7 +71,9 @@ void groupCustomersByHashtags(fileIterator& hashtags, fileIterator& purchases,fi
     #PROMPT# Create an unordered_map<int, unordered_map<string, int>> to store the count
      of hashtags for each customer. Read from the purchases file iterator and update the count.
     */
-    unordered_map<int, unordered_map<string, int>> hashtagCount;
+
+   // Using Global Variable hashtagCount
+
     while (purchases.hasNext()) {
         string line = purchases.next();
         if (line.empty()) break;
@@ -258,14 +262,133 @@ void groupCustomersByHashtagsForDynamicInserts(fileIterator& hashtags, fileItera
     //Use this to log compute time    
     auto start = high_resolution_clock::now();
     //  Write your code here
+    
+    // // Debug print hashtagCount
+    // for (auto it = hashtagCount.begin(); it != hashtagCount.end(); it++) {
+    //     cout << it->first << ":";
+    //     for (auto tag : it->second) {
+    //         cout << tag.first << " " << tag.second << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    #PROMPT# Read the new hashtags data using the newHashtags vector and store it in a suitable data structure.
+    There can be multiple hashtags for a product. Handle this in your data structure.
+    */
+
+    unordered_map<int, vector<string>> newHashtagsMap;
+    for (auto line : newHashtags) {
+        vector<string> tokens = split(line, ',');
+        int product_id = stoi(tokens[0]);
+        vector<string> hashtags(tokens.begin() + 1, tokens.end());
+        if(newHashtagsMap.find(product_id) != newHashtagsMap.end()){
+            newHashtagsMap[product_id].insert(newHashtagsMap[product_id].end(), hashtags.begin(), hashtags.end());
+        } else {
+            newHashtagsMap[product_id] = hashtags;
+        }
+    }
+
+    // // Debug
+    // for (auto it = newHashtagsMap.begin(); it != newHashtagsMap.end(); it++) {
+    //     cout << it->first << " ";
+    //     for (auto tag : it->second) {
+    //         cout << tag << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    #PROMPT# Update the hashtagCount data structure with the new hashtags for the products. Read the purchases file
+    and add all the newHashtags for that product to the customer's count.
+    */
+
+    while (purchases.hasNext()) {
+        string line = purchases.next();
+        if (line.empty()) break;
+        vector<string> tokens = split(line, ',');
+        int customer_id = stoi(tokens[0]);
+        int product_id = stoi(tokens[1]);
+        if (newHashtagsMap.find(product_id) != newHashtagsMap.end()) {
+            for (auto hashtag : newHashtagsMap[product_id]) {
+                hashtagCount[customer_id][hashtag]++;
+            }
+        }
+    }
+
+    // // Debug
+    // for (auto it = hashtagCount.begin(); it != hashtagCount.end(); it++) {
+    //     cout << it->first << ":";
+    //     for (auto tag : it->second) {
+    //         cout << tag.first << " " << tag.second << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    #PROMPT# Determine primary interests for each customer. Sort in descending order of frequency and then second level
+    Use the same logic as Q1.
+    */
+
+    unordered_map<int, vector<string>> customerPrimaryInterests;
+    for (auto it = hashtagCount.begin(); it != hashtagCount.end(); it++) {
+        vector<pair<string, int>> tags(it->second.begin(), it->second.end());
+        sort(tags.begin(), tags.end(), [](const pair<string, int>& a, const pair<string, int>& b) {
+            if (a.second == b.second) {
+                return a.first < b.first;
+            }
+            return a.second > b.second;
+        });
+        vector<string> primaryInterests;
+        for (int i = 0; i < k && i < tags.size(); i++) {
+            primaryInterests.push_back(tags[i].first);
+        }
+        customerPrimaryInterests[it->first] = primaryInterests;
+    }
+
+    // // Debug
+    // for (auto it = customerPrimaryInterests.begin(); it != customerPrimaryInterests.end(); it++) {
+    //     cout << it->first << ":";
+    //     for (auto tag : it->second) {
+    //         cout << tag << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    /*
+    #PROMPT# Determine the interest groups based on primary interests. If two customers have same elements
+    in customerPrimaryInterests.second in any order, they are in the same group. Use the same logic as Q1.
+    */
+
+    unordered_map<string, vector<int>> groupMap;
+    for (auto it = customerPrimaryInterests.begin(); it != customerPrimaryInterests.end(); it++) {
+        vector<string> primaryInterests = it->second;
+        sort(primaryInterests.begin(), primaryInterests.end());
+        string key = "";
+        for (auto tag : primaryInterests) {
+            key += tag + ",";
+        }
+        groupMap[key].push_back(it->first);
+    }
+
+    // // Debug
+    // for (auto it = groupMap.begin(); it != groupMap.end(); it++) {
+    //     cout << it->first << ":";
+    //     for (auto customer : it->second) {
+    //         cout << customer << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
     cout << "Time taken by compute part of the function: "<< duration.count() << " microseconds" << endl;
-
-    // Use the below utility function to write the output to a file
-    // Call this function for every group as a vector of integers
-    vector<int> group;
-    writeOutputToFile(group, outputFilePath);
+    
+    for(auto it = groupMap.begin(); it != groupMap.end(); it++){
+        vector<int> group = it->second;
+        writeOutputToFile(group, outputFilePath);
+    }
     return;
     
 }
